b5663aef53b4af2640825aa6deb225d3f062e1d5
urtzi88 <urtzip@gmail.com>
2016-03-31 11:24:50 +0200

    Third iteration

--- exercise.rb
+++ exercise.rb
@@ -1,30 +1,19 @@
-def solve_cipher(input, shift)
+def solve_cipher(input)
 	message = []
 	message = input.split(//)
+	shift = -3
 	decripted_message = []
 	for letter in message
 		orig = letter.ord
 		if orig == 32
 			new_letter = orig
 		else
-			if shift > 0
-				if (orig + shift) > 122 && orig < 122 && orig > 97
-					new_letter = 97 + (orig - shift - 122)
-				elsif (orig + shift) > 90 && orig < 90 && orig > 65
-					new_letter = 65 + (orig - shift - 90)
-				else
-					new_letter = orig + shift
-				end
-			elsif shift < 0
-				if (orig + shift) < 97 && orig < 122 && orig > 97
-					new_letter = 122 - (orig + shift - 97)
-				elsif (orig + shift) < 65 && orig < 90 && orig > 65
-					new_letter = 90 - (orig + shift - 65)
-				else
-					new_letter = orig + shift
-				end
+			if (orig + shift) < 97 && orig < 122 && orig > 97
+				new_letter = 122 - (orig + shift - 97)
+			elsif (orig + shift) < 65 && orig < 90 && orig > 65
+				new_letter = 90 - (orig + shift - 65)
 			else
-				new_letter = oring
+				new_letter = orig + shift
 			end
 		end
 		letter = new_letter.chr
@@ -33,5 +22,4 @@ def solve_cipher(input, shift)
 	puts decripted_message.join
 end
 
-#solve_cipher("ifmmp");
-solve_cipher("p| uhdo qdph lv grqdog gxfn", -3)
+solve_cipher("p| uhdo qdph lv grqdog gxfn")

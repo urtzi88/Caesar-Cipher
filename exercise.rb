aa6cd9ce558226c105fd0af0d6f9733072767ac6
urtzi88 <urtzip@gmail.com>
2016-03-31 10:50:39 +0200

    First Iteration

--- exercise.rb
+++ exercise.rb
@@ -1,5 +1,20 @@
-def solve_cipher(input){
-	
-}
+def solve_cipher(input)
+	message = []
+	message = input.split(//)
+	decripted_message = []
+	for letter in message
+		orig = letter.ord
+		if orig == 97
+			new_letter = 122
+		elsif orig == 65
+			new_letter = 90
+		else				
+			new_letter = orig - 1
+		end
+		letter = new_letter.chr
+		decripted_message << letter
+	end
+	puts decripted_message.join
+end
 
 solve_cipher("ifmmp");
